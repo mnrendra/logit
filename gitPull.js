fab1ebfec5290d28ed0a53d61dd19fa3352ca045
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-02 21:39:10 +0700

    fixing secod commit

--- gitPull.js
+++ gitPull.js
@@ -28,6 +28,7 @@ const gitPull = async (remote = '', branch = '') => {
 
     if (message.includes('CONFLICT') || message.includes('fatal') || message.includes('hint: ')) {
       console.log(message)
+      console.log('Please \'add\' and \'commit\' or \'stash\' your current change first!'.yellow)
       process.exit()
     }
 
